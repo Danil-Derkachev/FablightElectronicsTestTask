2b49d441a0bc0c02d55d0bdd2f28fd2184124a50
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-13 18:43:59 +0300

    Добавил эндпоинт user_create

--- fablight_electronics/user_api/urls.py
+++ fablight_electronics/user_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import UserList, UserRetrieveUpdateDestroy, UserByToken
+from .views import UserList, UserRetrieveUpdateDestroy, UserByToken, UserCreate
 
 urlpatterns = [
     path('user/by/token/', UserByToken.as_view(), name='user_by_token'),
     path('users', UserList.as_view(), name='users_list'),
     path('users/<int:pk>', UserRetrieveUpdateDestroy.as_view(), name='user_retrieve_update_destroy'),
+    path('users/create', UserCreate.as_view(), name='user_create'),
 ]
