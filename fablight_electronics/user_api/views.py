ccda292bd95f26cda4b971ed7beb420e8afc69aa
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-13 16:57:23 +0300

    Добавил представление UserList

--- fablight_electronics/user_api/views.py
+++ fablight_electronics/user_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import generics
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        data = UserSerializer(queryset, many=True).data
+        return JsonResponse(data=data, safe=False)
