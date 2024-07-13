adae7084705058026f627f479cd47c874584268f
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-13 17:16:15 +0300

    Добавил эндпоинт user_retrieve_update_destroy

--- fablight_electronics/user_api/urls.py
+++ fablight_electronics/user_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserList
+from .views import UserList, UserRetrieveUpdateDestroy
 
 urlpatterns = [
-    path('users', UserList.as_view(), name='users_list')
+    path('users', UserList.as_view(), name='users_list'),
+    path('users/<int:pk>', UserRetrieveUpdateDestroy.as_view(), name='user_retrieve_update_destroy'),
 ]
